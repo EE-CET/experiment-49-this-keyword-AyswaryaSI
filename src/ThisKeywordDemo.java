60296f3c32059d1cfeb92a51b91c90a6b995c6fe
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-06 11:45:56 +0530

    Fix class name in main method instantiation

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
 
 public class ThisKeywordDemo {
-
     int value;
-
     void setValue(int value) {
-        this.value = value;
+        this.value = value; 
     }
-
     void display() {
         System.out.println("Value: " + value);
     }
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int val = sc.nextInt();
 
-        ThisDemo obj = new ThisDemo();
+        ThisKeywordDemo obj = new ThisKeywordDemo();
         obj.setValue(val);
         obj.display();
 
